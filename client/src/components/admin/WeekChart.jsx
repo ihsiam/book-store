3c895dcfedab71b58f4ec079625f28ab9d806841
kmsayemuddin <musacse10@gmail.com>
2025-05-18 22:53:13 +0600

    admin chart implemennt

--- client/src/components/admin/WeekChart.jsx
+++ client/src/components/admin/WeekChart.jsx
@@ -48,9 +48,7 @@ const data = [
 export default function WeekChart() {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <BarChart width={800} height={400} data={data}>
-        <Bar dataKey="uv" fill="#8884d8" />
-      </BarChart>
+      
     </ResponsiveContainer>
   );
 }
